026f1d95ffdde2275335875bfa4af1ae15e42f53
Kajal Pandya <kajal@7span.com>
2023-10-27 15:11:44 +0530

    Removed commented code

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Generate Rest API files js -->
 <script src="{!! asset('js/rest/make-file.js') !!}"></script>
 <script src="{!! asset('js/rest/add-field.js') !!}"></script>
-<script src="{!! asset('js/rest/add-column-type-change.js') !!}"></script>
 <script src="{!! asset('js/rest/edit-field.js') !!}"></script>
+<script src="{!! asset('js/rest/add-column-type-change.js') !!}"></script>
 <script src="{!! asset('js/rest/edit-column-type-change.js') !!}"></script>
 <script src="{!! asset('js/rest/on-edit-or-delete-button.js') !!}"></script>
 <script src="{!! asset('js/rest/add-or-edit-more.js') !!}"></script>
@@ -10,10 +10,10 @@
 <script src="{!! asset('js/rest/add-notification-field.js') !!}"></script>
 
 <!-- Generate GraphQL type js -->
-<script src="{!! asset('js/type/make-type.js') !!}"></script>
+<script src="{!! asset('js/type/script.js') !!}"></script>
 
 <!-- Generate GraphQL query js -->
-<script src="{!! asset('js/query/make-query.js') !!}"></script>
+<script src="{!! asset('js/query/script.js') !!}"></script>
 
 <!-- Generate GraphQL mutation js -->
-<script src="{!! asset('js/mutation/make-mutation.js') !!}"></script>
+<script src="{!! asset('js/mutation/script.js') !!}"></script>

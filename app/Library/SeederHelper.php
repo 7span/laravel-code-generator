9333bb0ef5d852925ffb3731cbfcc5a6bbaf5ed5
charmy-7span <charmy@7span.com>
2024-02-09 11:16:28 +0530

    Add migration in database folder.

--- app/Library/SeederHelper.php
+++ app/Library/SeederHelper.php
@@ -2,17 +2,17 @@
 
 namespace App\Library;
 
-use File;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 
 class SeederHelper
 {
-    public static function makeSeeder($modelName, $data, $generatedFilesPath)
+    public static function makeSeeder($modelName, $generatedFilesPath)
     {
         // Make seeder using command and move it to Generated_files
-        \Artisan::call('make:seeder ' . $modelName . 'Seeder --data="' . json_encode($data) . '"');
+        Artisan::call('make:seeder ' . $modelName . 'Seeder');
         Storage::disk('local')->makeDirectory($generatedFilesPath . '/database/seeders');
-        File::move(database_path('seeders/' . $modelName . 'Seeder.php'), storage_path('app/' . $generatedFilesPath . '/database/seeders/' . $modelName . 'Seeder.php'));
+        File::move(base_path('database/seeders/' . $modelName . 'Seeder.php'), storage_path('app/' . $generatedFilesPath . '/database/seeders/' . $modelName . 'Seeder.php'));
     }
 }

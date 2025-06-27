58e86f7fad624bf4d0d3f9632c51c5cd4cf892d1
dhaval-j-r-7span <dhaval.j.r@7span.com>
2025-06-27 18:31:47 +0530

    changed data type options matches sqlquery for edit

--- resources/views/components/data-type-option.blade.php
+++ resources/views/components/data-type-option.blade.php
@@ -1,43 +1,64 @@
 <div>
     <option value="">Select one</option>
-    <optgroup label="String fields">
-        <option value="lineString">Line String</option>
-        <option value="multiLineString">Multi Line String</option>
-        <option value="string">String</option>
+    <optgroup label="Common fields">
+        <option value="int">INT</option>
+        <option value="varchar">VARCHAR</option>
+        <option value="text">TEXT</option>
+        <option value="date">DATE</option>
     </optgroup>
 
-    <optgroup label="Text fields">
-        <option value="mediumText">Medium text</option>
-        <option value="longText">Long text</option>
-        <option value="text">Text</option>
-        <option value="tinyText">Tiny text</option>
+    <optgroup label="Numeric">
+        <option value="tinyint">TINYINT</option>
+        <option value="smallint">SMALLINT</option>
+        <option value="mediumint">MEDIUMINT</option>
+        <option value="int">INT</option>
+        <option value="bigint">BIGINT</option>
+        <option value="decimal">DECIMAL</option>
+        <option value="float">FLOAT</option>
+        <option value="double">DOUBLE</option>
+        <option value="real">REAL</option>
+        <option value="bit">BIT</option>
+        <option value="boolean">BOOLEAN</option>
+        <option value="serial">SERIAL</option>
     </optgroup>
 
-    <optgroup label="Integer fields">
-        <option value="bigInteger">Big Integer</option>
-        <option value="integer">Integer</option>
-        <option value="mediumInteger">Medium Integer</option>
-        <option value="smallInteger">Small Integer</option>
-        <option value="tinyInteger">Tiny Integer</option>
+    <optgroup label="Date and time">
+        <option value="date">DATE</option>
+        <option value="datetime">DATETIME</option>
+        <option value="timestamp">TIMESTAMP</option>
+        <option value="time">TIME</option>
+        <option value="year">YEAR</option>
     </optgroup>
 
-    <optgroup label="Unsigned Integer fields">
-        <option value="unsignedBigInteger">Unsigned Big Integer</option>
-        <option value="unsignedInteger">Unsigned Integer</option>
-        <option value="unsignedMediumInteger">Unsigned Medium Integer</option>
-        <option value="unsignedSmallInteger">Unsigned Small Integer</option>
-        <option value="unsignedTinyInteger">Unsigned Tiny Integer</option>
+    <optgroup label="String">
+        <option value="char">CHAR</option>
+        <option value="varchar">VARCHAR</option>
+        <option value="tinytext">TINYTEXT</option>
+        <option value="text">TEXT</option>
+        <option value="mediumtext">MEDIUMTEXT</option>
+        <option value="longtext">LONGTEXT</option>
+        <option value="binary">BINARY</option>
+        <option value="varbinary">VARBINARY</option>
+        <option value="tinyblob">TINYBLOB</option>
+        <option value="blob">BLOB</option>
+        <option value="mediumblob">MEDIUMBLOB</option>
+        <option value="longblob">LONGBLOB</option>
+        <option value="enum">ENUM</option>
+        <option value="set">SET</option>
     </optgroup>
 
-    <optgroup label="Other fields">
-        <option value="boolean">Boolean</option>
-        <option value="decimal">Decimal</option>
-        <option value="double">Double</option>
-        <option value="float">Float</option>
-        <option value="enum">Enum</option>
-        <option value="uuid">UUID</option>
-        <option value="date">Date</option>
-        <option value="foreignKey">Foreign Key</option>
+    <optgroup label="Spatial">
+        <option value="geometry">GEOMETRY</option>
+        <option value="point">POINT</option>
+        <option value="linestring">LINESTRING</option>
+        <option value="polygon">POLYGON</option>
+        <option value="multipoint">MULTIPOINT</option>
+        <option value="multilinestring">MULTILINESTRING</option>
+        <option value="multipolygon">MULTIPOLYGON</option>
+        <option value="geometrycollection">GEOMETRYCOLLECTION</option>
     </optgroup>
-
+    
+    <optgroup label="Other">
+        <option value="json">JSON</option>
+    </optgroup>
 </div>
